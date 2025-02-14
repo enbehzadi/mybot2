d26cf0ee5c8b9f2ac4043905d241b097a4b8e3c9
Mojtaba Behzadi <en.behzadi@gmail.com>
2025-02-14 17:51:03 +0330

    change 35

--- app.py
+++ app.py
@@ -16,10 +16,9 @@ if not DATABASE_URL:
 def get_db_connection():
     try:
         conn = psycopg2.connect(DATABASE_URL)
-        print("Database connected successfully.")  # چاپ برای بررسی اتصال
         return conn
     except Exception as e:
-        print(f"Database connection error: {e}")
+        print("Database connection error:", e)
         return None
 
 @app.route('/')
