311eb9b3e05a38ea160323156b870f7fc6c3db19
Mojtaba Behzadi <en.behzadi@gmail.com>
2025-02-17 00:37:19 +0330

    change 48

--- bot.py
+++ bot.py
@@ -3,7 +3,9 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 import requests
 import os
 
-TOKEN = '7322999847:AAF5YpZDm4vGEmLWPGB6ht2lCcFmm6g5Ixs'
+TOKEN = os.getenv('7322999847:AAF5YpZDm4vGEmLWPGB6ht2lCcFmm6g5Ixs')
+if not TOKEN:
+    print("Error: Telegram bot token is missing!")
 API_URL = os.getenv('API_URL', 'https://web-production-445f.up.railway.app/messages')
 
 async def start(update: Update, context: CallbackContext):
@@ -27,15 +29,31 @@ async def add_message(update: Update, context: CallbackContext):
         else:
             await update.message.reply_text('❌ خطا در ارسال پیام.')
     except Exception as e:
+        print(f"Error sending message to API: {e}")
         await update.message.reply_text(f'❌ خطای سرور: {str(e)}')
 
 async def send_message_to_user(context: CallbackContext, chat_id: int, message: str):
     try:
+        chat_id = int(chat_id)
+    except ValueError:
+        print("Invalid chat_id")
+        return
+
+    if not TOKEN:
+        print("Error: Telegram bot token is missing!")
+        return
+
+    try:
+        print(f"Sending message to chat_id: {chat_id}, text: {message}")
         await context.bot.send_message(chat_id=chat_id, text=message)
     except Exception as e:
         print(f"Error sending message: {e}")
 
 def main():
+    if not TOKEN:
+        print("Error: Telegram bot token is missing!")
+        return
+
     application = Application.builder().token(TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
